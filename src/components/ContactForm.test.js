b4f7ea647b690d9ef4d9188c2a06f30b0cff2bbe
tarmes <traarmes@gmail.com>
2020-10-01 18:45:29 -0400

    grabbed labels

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -6,5 +6,16 @@ describe('Tests ContactForm', () => {
     test('ContactForm renders without errors', () => {
         render(<ContactForm/>)
     })
+    test('User can fill out and submit form', () => {
+        render(<ContactForm/>)
+
+        //Act
+        const firstNameInput = screen.getByLabelText(/first name/i)
+        const lastNameInput = screen.getByLabelText(/last name/i)
+        const emailInput = screen.getByLabelText(/email/i)
+        const messageInput = screen.getByLabelText(/message/i)
+
+        
+    })
 })
  
